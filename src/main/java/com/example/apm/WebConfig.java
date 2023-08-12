7749b672dd76e82c171890e24d0bc36c12930c7d
Skadi <74538745+SeokBaeJeon@users.noreply.github.com>
2023-08-12 18:36:26 +0900

    Update WebConfig.java

--- src/main/java/com/example/apm/WebConfig.java
+++ src/main/java/com/example/apm/WebConfig.java
@@ -1,19 +1,19 @@
 package com.example.apm;
 
-import io.netty.handler.codec.http.HttpMethod;
+
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*") // 실제 프론트엔드 주소
-                .allowedMethods("*") // 필요한 메소드만 허용
-                .allowedHeaders("*") // 모든 헤더 허용
-                .exposedHeaders(HttpHeaders.LOCATION); // 노출할 헤더 설정
+                .allowedOrigins("*") // 허용할 프론트엔드 주소
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .exposedHeaders(HttpHeaders.LOCATION);
     }
 }
