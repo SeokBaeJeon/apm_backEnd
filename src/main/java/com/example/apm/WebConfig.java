d8e82d857a8b5dc6f50db7ce3bc80a5307f87a00
Skadi <74538745+SeokBaeJeon@users.noreply.github.com>
2023-08-12 18:24:38 +0900

    Update WebConfig.java

--- src/main/java/com/example/apm/WebConfig.java
+++ src/main/java/com/example/apm/WebConfig.java
@@ -11,8 +11,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://apm-frontend-bc59a307af1a.herokuapp.com") // 실제 프론트엔드 주소
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 필요한 메소드만 허용
+                .allowedOrigins("*") // 실제 프론트엔드 주소
+                .allowedMethods("*") // 필요한 메소드만 허용
                 .allowedHeaders("*") // 모든 헤더 허용
                 .exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Headers", "Origin", "Content-Type", "Accept"); // 노출할 헤더 설정
     }
